1933dbed25094f080509fff6d6e28f5d3e79063f
Aanuoluwapo <aanuoluwaff@gmail.com>
2024-06-05 15:21:58 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+});
+
+const navKey = document.querySelectorAll(".nav-key");
+
+navKey.forEach((link) => link.addEventListener("click", () => {
+    ul.classList.remove("show");
+})
+);
